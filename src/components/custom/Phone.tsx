f867606ce8a2959cf9e0f5bbc12372523c44591d
flan02 <chanivetdan@hotmail.com>
2024-07-12 01:46:57 -0300

    Completed themes, landing page, upload dummy photos. Following steps authentication with Kinde at 005500

---
+++ src/components/custom/Phone.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> { // ? Inherits all HTMLDivElement props
+  // I don't need declare className, because is inherited from HTMLAttributes<HTMLDivElement>
+  imgSrc: string
+  dark?: boolean
+}
+
+const Phone = ({ className, imgSrc, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn("relative pointer-events-none z-50 overflow-hidden", className)}
+      {...props}
+    >
+      <img
+        src={dark
+          ? '/phone-template-dark-edges.png'
+          : '/phone-template-white-edges.png'}
+        className='pointer-events-none z-50 select-none'
+        alt='phone image'
+      />
+      <div className="absolute -z-10 inset-0">
+        <img
+          src={imgSrc}
+          alt="overlaying phone image"
+          className='object-cover'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Phone
