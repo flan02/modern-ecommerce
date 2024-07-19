b14a200acd945699e0cf23eed0b0ebb7b9892c6c
flan02 <chanivetdan@hotmail.com>
2024-07-18 22:24:22 -0300

    Our app were deployed on Vercel accordingly as we expected, I changed kinde api url as well as stripe api url and I had success buying my custom case Stripe allow me to complete the operation and after that I was redirected to thankyou page. At 092700.

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ThankyouPage = (props: Props) => {
+  return (
+    <div>ThankyouPage for bought our products.</div>
+  )
+}
+
+export default ThankyouPage
